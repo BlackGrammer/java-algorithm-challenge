b593afc76207decfaeebe8e3511c347d19b41bfc
blackgrammer <blackgrammer@gmail.com>
2019-11-13 22:44:20 +0900

    프린터 문제 불필요 메서드 제거

--- src/org/blackgrammer/heap/problem1/Solution.java
+++ src/org/blackgrammer/heap/problem1/Solution.java
@@ -31,9 +31,4 @@ public class Solution {
 
         return answer;
     }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.solution(new int[]{1, 2, 3, 9, 10, 12}, 7));
-    }
 }
