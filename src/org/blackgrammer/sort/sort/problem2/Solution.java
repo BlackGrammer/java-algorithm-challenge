54ab45eddf0e8626a8c4266ec48d3497cf91cd73
blackgrammer <blackgrammer@gmail.com>
2019-12-27 01:42:06 +0900

    가장 큰 수 문제 예외 케이스 해결 및 로직 단순화

--- src/org/blackgrammer/sort/sort/problem2/Solution.java
+++ src/org/blackgrammer/sort/sort/problem2/Solution.java
@@ -12,7 +12,6 @@ import java.util.*;
 public class Solution {
 
     public String solution(int[] numbers) {
-
         List<String> strList = new ArrayList<>();
 
         for (int number : numbers) {
@@ -20,26 +19,9 @@ public class Solution {
         }
 
         strList.sort((str1, str2) -> {
-            int pos1 = 0;
-            int pos2 = 0;
-            boolean firstOutbound = false;
-            boolean secondOutbound = false;
-
-            while (true) {
-                char ch1 = firstOutbound ? str1.charAt(0) : str1.charAt(pos1);
-                char ch2 = secondOutbound ? str2.charAt(0) : str2.charAt(pos2);
-
-                if (ch1 == ch2) {
-                    if (pos1 + 1 < str1.length()) pos1++;
-                    else firstOutbound = true;
-                    if (pos2 + 1 < str2.length()) pos2++;
-                    else secondOutbound = true;
-
-                    if (firstOutbound && secondOutbound) return str2.length() - str1.length();
-                    continue;
-                }
-                return ch2 - ch1;
-            }
+            String comb1 = str1 + str2;
+            String comb2 = str2 + str1;
+            return Integer.parseInt(comb2) - Integer.parseInt(comb1);
         });
 
         StringBuilder answer = new StringBuilder();
@@ -47,16 +29,9 @@ public class Solution {
             answer.append(str);
         }
 
+        if (answer.charAt(0) == '0') return "0";
+
         return answer.toString();
     }
 
-    public static void main(String[] args) {
-//            [3, 30, 34, 33, 5, 9]
-//        "953433330"
-
-//                [3, 30, 34, 5, 9]
-//        "9534330"
-        Solution s = new Solution();
-        System.out.println(s.solution(new int[]{3, 30, 34, 33, 5, 9}));
-    }
 }
