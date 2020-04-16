9e853e4389c714ff39a69e2fd90a6147c9e6d2da
blackgrammer <blackgrammer@gmail.com>
2020-04-16 13:37:10 +0900

    소수 찾기 문제 해결

--- src/org/blackgrammer/bruteforce/problem2/Solution.java
+++ src/org/blackgrammer/bruteforce/problem2/Solution.java
@@ -1,9 +1,6 @@
 package org.blackgrammer.bruteforce.problem2;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 소수찾기 _ 프로그래머스 _ 완전탐색
@@ -15,24 +12,39 @@ import java.util.Set;
 public class Solution {
 
     public int solution(String numbers) {
-
-        int size = numbers.length();
-        Set<Integer> decimalStore = new HashSet<>();
-
-        for (int length = 1; length <= size; length++) {
-            decimalStore.addAll(getDecimalNumList(numbers, length));
+        List<String> numberList = Arrays.asList(numbers.split(""));
+        Set<Integer> decimalList = new HashSet<>();
+        getDecimalAndAdd(numberList, decimalList, "");
+        for (Integer decimal : decimalList) {
+            System.out.println(decimal);
         }
-
-        return decimalStore.size();
+        return decimalList.size();
     }
 
-    private List<Integer> getDecimalNumList(String numbers, int length) {
-        List<Integer> decimalNumList = new ArrayList<>();
-        for (int idx = 0, size = numbers.length(); idx < size; idx++) {
-
+    private void getDecimalAndAdd(List<String> numberList, Set<Integer> decimalList, String prevNumber) {
+        int index = 0;
+        for (String number : numberList) {
+            List<String> cloneNumberList = new ArrayList<>(numberList);
+            cloneNumberList.remove(index);
+            String newPrevNumber = number + prevNumber;
+            if (isDecimal(number, prevNumber)) {
+                decimalList.add(Integer.parseInt(newPrevNumber));
+            }
+            getDecimalAndAdd(cloneNumberList, decimalList, newPrevNumber);
+            index++;
         }
+    }
 
-        return decimalNumList;
+    private boolean isDecimal(String number, String privateDecimal) {
+        Integer target = privateDecimal.isEmpty() ? Integer.parseInt(number) : Integer.parseInt(number + privateDecimal);
+        if (target.equals(0) || target.equals(1)) return false;
+        if (target.equals(2)) return true;
+        int count = 1;
+        for (int i = 2, max = target / 2; i <= max; i++) {
+            if (target % i == 0) count++;
+            if (count >= 2) return false;
+        }
+        return true;
     }
 
 
