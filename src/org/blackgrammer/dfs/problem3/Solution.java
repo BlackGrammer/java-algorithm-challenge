3c653be9513ac48fabe1414b3107e75621768b46
blackgrammer <blackgrammer@gmail.com>
2020-04-22 12:18:14 +0900

    단어변환문제 풀이완성

--- src/org/blackgrammer/dfs/problem3/Solution.java
+++ src/org/blackgrammer/dfs/problem3/Solution.java
@@ -1,5 +1,6 @@
 package org.blackgrammer.dfs.problem3;
 
+import javax.xml.transform.SourceLocator;
 import java.util.*;
 
 /**
@@ -10,50 +11,61 @@ import java.util.*;
  */
 public class Solution {
 
-    /*
-        #### 제한사항
-        * 각 단어는 알파벳 소문자로만 이루어져 있습니다.
-        * 각 단어의 길이는 3 이상 10 이하이며 모든 단어의 길이는 같습니다.
-        * words에는 3개 이상 50개 이하의 단어가 있으며 중복되는 단어는 없습니다.
-        * begin과 target은 같지 않습니다.
-        * 변환할 수 없는 경우에는 0를 return 합니다.
-
-        #### 입출력 예
-        begin   | target        | words                                         | return
-        ---     | ---           | ---                                           | ---
-        hit     |	"cog"       |	["hot", "dot", "dog", "lot", "log", "cog"]  |	4
-        hit     |	"cog"       |	["hot", "dot", "dog", "lot", "log"]	        |   0
-     */
-
     public int solution(String begin, String target, String[] words) {
 
-
-        int minCaseCnt = 0;
         Set<Integer> caseCntSet = new HashSet<>();
 
         for (int i = 0, len = words.length; i < len; i++) {
-            boolean[] visits = new boolean[words.length];
+            boolean[] visits = new boolean[len];
             if (getDiffCnt(begin, words[i]) == 1) {
-                visits[i] = true;
-                getCaseCnt(words[i], words, target, visits, caseCntSet);
+                getCaseCnt(i, words, target, visits, caseCntSet);
             }
         }
-        return minCaseCnt;
+        return getMinValue(caseCntSet);
     }
 
-    private void getCaseCnt(String word, String[] words, String target, boolean[] visits, Set<Integer> caseCntSet) {
-        for (int i = 0, len = visits.length; i < len; i++) {
-//            if (visits[i])
+    private void getCaseCnt(int index, String[] words, String target, boolean[] visits, Set<Integer> caseCntSet) {
+
+        visits[index] = true;
+
+        if (words[index].equals(target)) {
+            caseCntSet.add(getVisitCnt(visits));
+            return;
+        }
+
+        for (int i = 0, len = words.length; i < len; i++) {
+            if (getDiffCnt(words[index], words[i]) == 1 && !visits[i]) {
+                boolean[] visitsCopy = new boolean[len];
+                System.arraycopy(visits, 0, visitsCopy, 0, len);
+                getCaseCnt(i, words, target, visitsCopy, caseCntSet);
+            }
         }
     }
 
 
     private int getDiffCnt(String target, String word) {
+
         int diffCnt = 0;
         for (int i = 0, len = word.length(); i < len; i++) {
             if (target.charAt(i) != word.charAt(i)) diffCnt++;
         }
         return diffCnt;
+    }
+
+    private int getVisitCnt(boolean[] visits) {
+        int cnt = 0;
+        for (boolean visit : visits) {
+            if (visit) cnt++;
+        }
+        return cnt;
+    }
+
+    private int getMinValue(Set<Integer> cntSet) {
+        int minValue = 0;
+        for (int val : cntSet) {
+            if (val < minValue || minValue == 0) minValue = val;
+        }
+        return minValue;
     }
 
 }
