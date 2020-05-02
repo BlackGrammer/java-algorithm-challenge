a6342206ae9f6f2a8d417cbd28a7a7c71094a9c6
blackgrammer <blackgrammer@gmail.com>
2020-05-02 19:42:38 +0900

    징검다리 문제 추가 (문제만)

---
+++ src/org/blackgrammer/binary/problem3/Solution.java
@@ -0,0 +1,15 @@
+package org.blackgrammer.binary.problem3;
+
+/**
+ * 징검다리 _ 프로그래머스 _ 이분탐색
+ *
+ * @author blackgrammer
+ * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/43236">프로그래머스</a>
+ *
+ */
+public class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        return answer;
+    }
+}
