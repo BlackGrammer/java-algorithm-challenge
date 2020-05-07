005921543a6095f1d97cc7bdf9ee39e276a9d11a
blackgrammer <blackgrammer@gmail.com>
2020-05-07 21:36:14 +0900

    징검다리 이분탐색에 맞게 변경 테스트케이스 2번만 실패..

--- src/org/blackgrammer/binary/problem3/Solution.java
+++ src/org/blackgrammer/binary/problem3/Solution.java
@@ -11,73 +11,33 @@ import java.util.Arrays;
 public class Solution {
     public int solution(int distance, int[] rocks, int n) {
         Arrays.sort(rocks);
+        int left = 0;
+        int right = distance;
+        int middle = distance / 2;
 
-        int[] diffArr = new int[rocks.length + 3];
-        diffArr[0] = 0;
-        diffArr[rocks.length + 2] = 0;
-        int idx = 1;
-        int prevPos = 0;
-        for (int rock : rocks) {
-            diffArr[idx] = rock - prevPos;
-            prevPos = rock;
-            idx++;
-        }
-        diffArr[rocks.length + 1] = distance - prevPos;
+        while (true) {
+            int prevPos = 0;
+            int numberOfEliminate = 0;
+            int minDistance = 0;
 
-        while (n > 0) {
-            int minPos = 1;
-            int minNeighborPos = 2;
-            int minSum = diffArr[1] + diffArr[2];
-            for (int i = 2, len = diffArr.length - 1; i < len; i++) {
-                int target = diffArr[i];
-                int nextVal = diffArr[i + 1];
-                int prevVal = diffArr[i - 1];
-                if (diffArr[minPos] > target) {
-                    minPos = i;
-                    if (prevVal == 0) minNeighborPos = i + 1;
-                    else if (nextVal == 0) minNeighborPos = i - 1;
-                    else minNeighborPos = prevVal < nextVal ? i - 1 : i + 1;
-                    minSum = target + diffArr[minNeighborPos];
-                } else if (diffArr[minPos] == target) {
-                    int targetNeighbor;
-                    if (prevVal == 0) targetNeighbor = i + 1;
-                    else if (nextVal == 0) targetNeighbor = i - 1;
-                    else targetNeighbor = prevVal < nextVal ? i - 1 : i + 1;
-                    if (minSum > target + diffArr[targetNeighbor]) {
-                        minPos = i;
-                        minNeighborPos = targetNeighbor;
-                        minSum = target + diffArr[targetNeighbor];
-                    }
+            for (int rock : rocks) {
+                int targetDistance = rock - prevPos;
+                if (targetDistance < middle) numberOfEliminate++;
+                else {
+                    minDistance = minDistance == 0 ? targetDistance : Math.min(minDistance, targetDistance);
+                    prevPos = rock;
                 }
-
             }
 
-            int[] newDiffArr = new int[diffArr.length - 1];
-            int newIndex = 0;
-            for (int i = 0, len = diffArr.length; i < len; i++) {
-                if (i == minPos) {
-                    if (minPos > minNeighborPos) {
-                        newDiffArr[newIndex - 1] = minSum;
-                        continue;
-                    } else {
-                        newDiffArr[newIndex] = minSum;
-                        i++;
-                    }
-                } else {
-                    newDiffArr[newIndex] = diffArr[i];
-                }
-                newIndex++;
-            }
-            diffArr = newDiffArr;
-            n--;
-        }
+            int lastDistance = distance - prevPos;
+            if (lastDistance < middle) numberOfEliminate++;
 
-        int minDistance = diffArr[1];
-        for (int diff : diffArr) {
-            if (diff == 0) continue;
-            minDistance = Math.min(minDistance, diff);
+            if (numberOfEliminate == n || Math.abs(left - right) <= 1) return Math.min(minDistance, lastDistance);
+
+            if (numberOfEliminate > n) right = middle;
+            else left = middle;
+            middle = (left + right) / 2;
         }
-        return minDistance;
     }
 
     public static void main(String[] args) {
@@ -86,5 +46,9 @@ public class Solution {
         System.out.println(s.solution(25, new int[]{3, 6, 10, 15, 23}, 1)); // 3
         System.out.println(s.solution(25, new int[]{3, 6, 10, 15, 23}, 2)); // 4
         System.out.println(s.solution(25, new int[]{3, 6, 10, 15, 23}, 3)); // 6
+        System.out.println(s.solution(12, new int[]{2, 4, 6, 8, 10}, 3)); // 4
+        System.out.println(s.solution(12, new int[]{2, 4, 6, 8, 10}, 4)); // 6
+        System.out.println(s.solution(12, new int[]{2, 4, 6, 8, 10}, 1)); // 2
+        System.out.println(s.solution(11, new int[]{2, 4, 6, 8, 10}, 1)); // 2
     }
 }
