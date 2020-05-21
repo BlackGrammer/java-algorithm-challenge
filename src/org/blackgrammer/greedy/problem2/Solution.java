2d23e0e87a15ed1ad8a9b0e2c757f3be39ad86b4
blackgrammer <blackgrammer@gmail.com>
2020-05-22 01:42:34 +0900

    큰 수 만들기 문제 해결 (StringBuilder 로 바꾸니 해결)

--- src/org/blackgrammer/greedy/problem2/Solution.java
+++ src/org/blackgrammer/greedy/problem2/Solution.java
@@ -13,19 +13,19 @@ public class Solution {
         int length = number.length();
         int maxIdx = 0;
         int remain = length - k;
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
 
         while(true) {
             for(int i = maxIdx; i < length - remain + 1; i++ ){
                 if(number.charAt(maxIdx) < number.charAt(i)) maxIdx = i;
             }
-            answer += number.charAt(maxIdx);
+            answer.append(number.charAt(maxIdx));
             maxIdx ++;
             remain --;
             if(answer.length() == length - k) break;
         }
 
-        return answer;
+        return answer.toString();
     }
 
 }
