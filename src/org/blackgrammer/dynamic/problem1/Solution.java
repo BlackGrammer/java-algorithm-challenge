0b7a96c9af42b2ed2790a7cb918ddd4b532bc7e9
blackgrammer <blackgrammer@gmail.com>
2020-06-04 00:38:10 +0900

    N으로 표현하기 문제 해결

--- src/org/blackgrammer/dynamic/problem1/Solution.java
+++ src/org/blackgrammer/dynamic/problem1/Solution.java
@@ -1,7 +1,6 @@
 package org.blackgrammer.dynamic.problem1;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * N으로 표현 _ 프로그래머스 _ 동적계획법
@@ -11,46 +10,53 @@ import java.util.Map;
  */
 public class Solution {
 
-    /*
-     * N은 1 이상 9 이하입니다.
-     * number는 1 이상 32,000 이하입니다.
-     * 수식에는 괄호와 사칙연산만 가능하며 나누기 연산에서 나머지는 무시합니다.
-     * 최솟값이 8보다 크면 -1을 return 합니다.
-     */
-
     public int solution(int N, int number) {
-        // 1  -- 5 / 5
-        // 2  -- (5 + 5) / 5
-        // 3  -- (5 + 5 + 5) / 5 == 5 - (5 + 5) / 5
-        // 4  -- 5 - 5 / 5
-        // 5  -- 5
-        // 6  -- 5 + 5 / 5
-        // 7  -- 5 + (5 + 5) / 5
-        // 10 -- 5 + 5
-        // 11 -- 55 / 5
-        // 12 -- (55 + 5) / 5
-        // 22 -- (55 + 55 ) /5
-        // 111 -- (555
+        if (N == number) return 1;
+        Map<Integer, Set<Integer>> caseStore = new HashMap<>();
+        caseStore.put(1, new HashSet<>(Collections.singletonList(N)));
 
-        Map<Integer, Integer> caseMap = new HashMap<>();
-        caseMap.put(N, 1);
-        for (int i = 1; i < N; i++) {
-            int cnt;
-            if (2 * i > N) {
-                cnt = N-i+2;
-            } else {
-                cnt = i + 1;
+        for (int i = 2; i <= 8; i++) {
+            Set<Integer> newCaseArr = new HashSet<>();
+            for (int i2 = 1; i2 <= i / 2; i2++) {
+                for (int prevCase1 : caseStore.get(i2)) {
+                    for (int prevCase2 : caseStore.get(i - i2)) {
+                        int plusResult = prevCase1 + prevCase2;
+                        if (plusResult == number) return i;
+                        newCaseArr.add(plusResult);
+
+                        int minusResult = prevCase1 - prevCase2;
+                        if (minusResult == number) return i;
+                        newCaseArr.add(minusResult);
+
+                        int reverseMinusResult = prevCase2 - prevCase1;
+                        if (reverseMinusResult == number) return i;
+                        newCaseArr.add(reverseMinusResult);
+
+                        int multiplyResult = prevCase1 * prevCase2;
+                        if (multiplyResult == number) return i;
+                        newCaseArr.add(multiplyResult);
+
+                        if (prevCase2 != 0) {
+                            int divideResult = prevCase1 / prevCase2;
+                            if (divideResult == number) return i;
+                            newCaseArr.add(divideResult);
+                        }
+
+                        if (prevCase1 != 0) {
+                            int reverseDivideResult = prevCase2 / prevCase1;
+                            if (reverseDivideResult == number) return i;
+                            newCaseArr.add(reverseDivideResult);
+                        }
+                    }
+                }
             }
-            caseMap.put(i, cnt);
+
+            int accumVal = Integer.parseInt(String.valueOf(N).repeat(i));
+            if (accumVal == number) return i;
+            newCaseArr.add(accumVal);
+            caseStore.put(i, newCaseArr);
         }
-
-        return 1;
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println("s.solution(5, 12) = " + s.solution(5, 12)); // 4 -- (55 + 5) / 5
-        System.out.println("s.solution(2, 11) = " + s.solution(2, 11)); // 3 -- 22 / 2
+        return -1;
     }
 
 }
