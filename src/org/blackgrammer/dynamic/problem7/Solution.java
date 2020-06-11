ff88734db098f90af6426c8d4e6a7191e4a27b01
blackgrammer <blackgrammer@gmail.com>
2020-06-11 10:40:15 +0900

    서울에서 경산까지 문제 추가

---
+++ src/org/blackgrammer/dynamic/problem7/Solution.java
@@ -0,0 +1,16 @@
+package org.blackgrammer.dynamic.problem7;
+
+
+/**
+ * 서울에서 경산까지 _ 프로그래머스 _ 동적계획법
+ *
+ * @author blackgrammer
+ * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/42899">프로그래머스</a>
+ */
+public class Solution {
+    public int solution(int K, int[][] travel) {
+        int answer = 0;
+        return answer;
+    }
+
+}
