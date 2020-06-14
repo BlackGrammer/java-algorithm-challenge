a8b2d58dfe9e764229f0f2201e8a8c74139bd559
blackgrammer <blackgrammer@gmail.com>
2020-06-14 21:09:59 +0900

    가장 먼 노드 문제 노드별 연결 노드 미리 계산하여 수행시간 최적화

--- src/org/blackgrammer/graph/problem1/Solution.java
+++ src/org/blackgrammer/graph/problem1/Solution.java
@@ -1,8 +1,7 @@
 package org.blackgrammer.graph.problem1;
 
 
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 가장 먼 노드 _ 프로그래머스 _ 그래프
@@ -13,20 +12,25 @@ import java.util.Queue;
 public class Solution {
     public int solution(int n, int[][] edges) {
         int[] distances = new int[n];
+        Map<Integer, Set<Integer>> vMap = new HashMap<>();
+
+        for (int[] edge : edges) {
+            if (vMap.get(edge[0]) == null) vMap.put(edge[0], new HashSet<>());
+            if (vMap.get(edge[1]) == null) vMap.put(edge[1], new HashSet<>());
+            if (edge[1] != 1) vMap.get(edge[0]).add(edge[1]);
+            if (edge[0] != 1) vMap.get(edge[1]).add(edge[0]);
+        }
 
         Queue<Integer> nextNodes = new ArrayDeque<>();
         nextNodes.offer(1);
+
         int max = 0;
         int cnt = 0;
         while (!nextNodes.isEmpty()) {
             int curr = nextNodes.poll();
-            for (int[] edge : edges) {
-                int target;
-                if (edge[0] == curr) target = edge[1];
-                else if (edge[1] == curr) target = edge[0];
-                else continue;
-
-                if (target == 1) continue;
+            Iterator<Integer> it = vMap.get(curr).iterator();
+            while (it.hasNext()) {
+                int target = it.next();
                 if (distances[target - 1] != 0) continue;
                 int newDistance = distances[curr - 1] + 1;
                 distances[target - 1] = newDistance;
