ae38376dca16aa83c6302cccc8888e3f4acffe14
blackgrammer <blackgrammer@gmail.com>
2020-06-14 20:53:01 +0900

    가장 먼 노드 문제 해결(시간이 좀 오래걸림..)

--- src/org/blackgrammer/graph/problem1/Solution.java
+++ src/org/blackgrammer/graph/problem1/Solution.java
@@ -1,6 +1,9 @@
 package org.blackgrammer.graph.problem1;
 
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * 가장 먼 노드 _ 프로그래머스 _ 그래프
  *
@@ -10,38 +13,31 @@ package org.blackgrammer.graph.problem1;
 public class Solution {
     public int solution(int n, int[][] edges) {
         int[] distances = new int[n];
-        findMaxDistanceNodes(edges, distances, 1);
 
+        Queue<Integer> nextNodes = new ArrayDeque<>();
+        nextNodes.offer(1);
         int max = 0;
         int cnt = 0;
-        for (int distance : distances) {
-            if (distance > max) {
-                max = distance;
-                cnt = 1;
-            } else if (distance == max) {
-                cnt++;
+        while (!nextNodes.isEmpty()) {
+            int curr = nextNodes.poll();
+            for (int[] edge : edges) {
+                int target;
+                if (edge[0] == curr) target = edge[1];
+                else if (edge[1] == curr) target = edge[0];
+                else continue;
+
+                if (target == 1) continue;
+                if (distances[target - 1] != 0) continue;
+                int newDistance = distances[curr - 1] + 1;
+                distances[target - 1] = newDistance;
+                if (newDistance > max) {
+                    max = newDistance;
+                    cnt = 1;
+                } else if (newDistance == max) cnt++;
+                nextNodes.offer(target);
             }
         }
+
         return cnt;
-    }
-
-    public void findMaxDistanceNodes(int[][] edges, int[] distances, int curr) {
-        for (int[] edge : edges) {
-            int target;
-            if (edge[0] == curr) target = edge[1];
-            else if (edge[1] == curr) target = edge[0];
-            else continue;
-
-            if (target == 1) continue;
-            if (distances[target - 1] > distances[curr - 1] + 1 || distances[target - 1] == 0) {
-                distances[target - 1] = distances[curr - 1] + 1;
-                findMaxDistanceNodes(edges, distances, target);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.solution(6, new int[][]{{3, 6}, {4, 3}, {3, 2}, {1, 3,}, {1, 2}, {2, 4}, {5, 2}})); // 3
     }
 }
