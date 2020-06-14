be770f61ac62b1794884653ad32735933dde6bcc
blackgrammer <blackgrammer@gmail.com>
2020-06-14 19:20:37 +0900

    가장 먼 노드 문제 추가

---
+++ src/org/blackgrammer/graph/problem1/Solution.java
@@ -0,0 +1,15 @@
+package org.blackgrammer.graph.problem1;
+
+
+/**
+ * 가장 먼 노드 _ 프로그래머스 _ 그래프
+ *
+ * @author blackgrammer
+ * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/49189">프로그래머스</a>
+ */
+public class Solution {
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        return answer;
+    }
+}
