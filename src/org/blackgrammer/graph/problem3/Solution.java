0f38f102748dbe6c26e89dc5fca06b2e9fee85a8
blackgrammer <blackgrammer@gmail.com>
2020-06-16 17:53:16 +0900

    사이클제거 문제 첫번째 계획 완성

--- src/org/blackgrammer/graph/problem3/Solution.java
+++ src/org/blackgrammer/graph/problem3/Solution.java
@@ -1,6 +1,9 @@
 package org.blackgrammer.graph.problem3;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 사이클 제거 _ 프로그래머스 _ 그래프
  *
@@ -10,7 +13,20 @@ package org.blackgrammer.graph.problem3;
 public class Solution {
 
     public int solution(int n, int[][] edges) {
+
+        Set<Integer> possible = new HashSet<Integer>();
+        for (int i = 1; i <= n; i++) {
+            // i 번 노드일경우 PASS -- edge 없다고 가정
+            // 노드들 dfs 로 탐색하면서 사이클 있는지 확인
+            // 사이클 존재한다면 set 에 추가
+        }
+
+        // 가능한 node 번호들 더하여 답 return
         int answer = 0;
+        for(int ans : possible) {
+            answer += ans;
+        }
+
         return answer;
     }
 
