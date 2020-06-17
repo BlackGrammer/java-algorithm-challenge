f155551663eb54de0529b70d2f1a3599d5cc726d
blackgrammer <blackgrammer@gmail.com>
2020-06-17 15:16:40 +0900

    사이클 제거 문제 첫번쨰 풀이완성 (시간초과 다수)

--- src/org/blackgrammer/graph/problem3/Solution.java
+++ src/org/blackgrammer/graph/problem3/Solution.java
@@ -1,8 +1,7 @@
 package org.blackgrammer.graph.problem3;
 
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 사이클 제거 _ 프로그래머스 _ 그래프
@@ -14,20 +13,66 @@ public class Solution {
 
     public int solution(int n, int[][] edges) {
 
-        Set<Integer> possible = new HashSet<Integer>();
-        for (int i = 1; i <= n; i++) {
-            // i 번 노드일경우 PASS -- edge 없다고 가정
-            // 노드들 dfs 로 탐색하면서 사이클 있는지 확인
-            // 사이클 존재한다면 set 에 추가
+        int notValid = 0;
+        int edgeCnt = edges.length;
+        Stack<Integer> nodeStack;
+        boolean[] visitNodes;
+        boolean[] visitEdges;
+
+        for (int remove = 1; remove <= n; remove++) {
+            // stack, marker 초기화
+            nodeStack = new Stack<>();
+            visitNodes = new boolean[n + 1];
+            visitEdges = new boolean[edges.length];
+
+            int startNode = ((remove + 1) % n) + 1;
+            nodeStack.push(startNode);
+            visitNodes[startNode] = true;
+
+            // stack 에 node 쌓아가면서 모든 노드 bfs
+            while (!nodeStack.isEmpty()) {
+                int bottom = nodeStack.peek();
+                boolean isCycle = false;
+                for (int edgeIdx = 0; edgeIdx < edgeCnt; edgeIdx++) {
+                    // 방문한 edge 는 넘어간다
+                    if (visitEdges[edgeIdx]) continue;
+
+                    int[] targetEdge = edges[edgeIdx];
+                    int node_1 = targetEdge[0];
+                    int node_2 = targetEdge[1];
+
+                    // remove 된 node 의 edge 는 넘어간다
+                    if (node_1 == remove || node_2 == remove) {
+                        visitEdges[edgeIdx] = true;
+                        continue;
+                    }
+                    if (node_1 == bottom || node_2 == bottom) {
+                        int targetNode = node_1 == bottom ? node_2 : node_1;
+                        if (visitNodes[targetNode]) {
+                            isCycle = true;
+                        }
+                        visitNodes[targetNode] = true;
+                        visitEdges[edgeIdx] = true;
+                        nodeStack.push(targetNode);
+                        break;
+                    }
+                }
+                if (isCycle) {
+                    notValid += remove;
+                    break;
+                }
+                if (nodeStack.peek() == bottom)
+                    nodeStack.pop();
+            }
         }
 
-        // 가능한 node 번호들 더하여 답 return
-        int answer = 0;
-        for(int ans : possible) {
-            answer += ans;
-        }
+        return n * (n + 1) / 2 - notValid;
+    }
 
-        return answer;
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(4, new int[][]{{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}})); // 5
+        System.out.println(s.solution(8, new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {7, 8}, {8, 1}, {2, 7}, {3, 6}})); // 0
     }
 
 }
