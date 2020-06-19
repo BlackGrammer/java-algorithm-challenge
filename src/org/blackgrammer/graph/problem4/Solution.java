4f4bacf536817eeb905de6c15204b2e1c16a4a40
blackgrammer <blackgrammer@gmail.com>
2020-06-19 11:02:08 +0900

    방의 개수 문제 첫번째 풀이 (edge 간의 교점 계산 추가 필요)

--- src/org/blackgrammer/graph/problem4/Solution.java
+++ src/org/blackgrammer/graph/problem4/Solution.java
@@ -1,6 +1,7 @@
 package org.blackgrammer.graph.problem4;
 
 
+import java.util.Arrays;
 
 /**
  * 방의 개수 _ 프로그래머스 _ 그래프
@@ -11,18 +12,52 @@ package org.blackgrammer.graph.problem4;
 public class Solution {
 
     public int solution(int[] arrows) {
-        int x = 0;
-        int y = 0;
         int arrowCnt = arrows.length;
-        int[][] visitNode = new int[arrowCnt][arrowCnt];
-        int[][] visitEdge = new int[arrowCnt][arrowCnt];
+        int[] point = new int[2];
+        boolean[][] visitNode = new boolean[arrowCnt * 2][arrowCnt * 2];
+        int[][] visitEdge = new int[arrowCnt * 2][arrowCnt * 2];
+        for (int[] edge : visitEdge) {
+            Arrays.fill(edge, -1);
+        }
+        Arrays.fill(point, arrowCnt);
+        visitNode[arrowCnt][arrowCnt] = true;
 
         int answer = 0;
+        for (int arrow : arrows) {
+            move(point, arrow);
+            // TODO: edge
+            if ((visitNode[point[0]][point[1]] )
+                    && visitEdge[point[0]][point[1]] != arrow) {
+                visitEdge[point[0]][point[1]] = arrow;
+                answer++;
+            } else {
+                visitNode[point[0]][point[1]] = true;
+                visitEdge[point[0]][point[1]] = arrow;
+            }
+        }
+
         return answer;
+    }
+
+    public void move(int[] currPoint, int caseNum) {
+        int moveX = 0;
+        int moveY = 0;
+        if (caseNum % 4 != 0) {
+            moveX = caseNum / 4 == 1 ? -1 : 1;
+        }
+        if ((caseNum + 2) % 4 != 0) {
+            moveY = caseNum / 4 == 1 ? -1 : 1;
+        }
+        currPoint[0] += moveX;
+        currPoint[1] += moveY;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
+        System.out.println(s.solution(new int[]{6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0})); // 3
+        System.out.println(s.solution(new int[]{6, 5, 2, 7, 1, 4, 2, 4, 6})); // 3
+        System.out.println(s.solution(new int[]{5, 2, 7, 1, 6, 3})); // 3
+        System.out.println(s.solution(new int[]{6, 2, 4, 0, 5, 0, 6, 4, 2, 4, 2, 0})); // 3
     }
 
 }
